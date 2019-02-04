6f5aeab14c0dc99d172557abc25979efa0a14ca1
thanos-kant <thanos.kantalaft@gmail.com>
2019-02-05 00:12:52 +0200

    Create a show template for posts- Add a show action and query a post to an instance variable- Create a show.scss file and add CSS

---
+++ db/migrate/20110608232732_create_lists.rb
@@ -0,0 +1,13 @@
+class CreateLists < ActiveRecord::Migration[5.1]
+  def self.up
+    create_table :lists do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :lists
+  end
+end
