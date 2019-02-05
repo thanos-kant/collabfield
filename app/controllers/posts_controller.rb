aeb0ef6497f86ce4b2e20baafdf6be177da23116
Manos Perris <manosfp97@gmail.com>
2019-02-05 23:24:34 +0200

    Inside a post add a form to contact a user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,9 @@
 class PostsController < ApplicationController
 def show
     @post = Post.find(params[:id])
+    if user_signed_in?
+      @message_has_been_sent = conversation_exist?
+    end
 end
 
 def get_posts
@@ -38,4 +41,9 @@ end
 def team
     posts_for_branch(params[:action])
 end
+
+def conversation_exist?
+  Private::Conversation.between_users(current_user.id, @post.user.id).present?
+end
+
 end
