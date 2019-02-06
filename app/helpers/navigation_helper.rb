993636978178105d95729473ea183e548352ad09
thanos-kant <thanos.kantalaft@gmail.com>
2019-02-06 14:55:47 +0200

    - Create a _contact_requests.html.erb inside thelayouts/navigation/header/dropdowns- Define a nav_contact_requests_partial_path in NavigationHelper

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -6,4 +6,12 @@ module NavigationHelper
       'layouts/navigation/collapsible_elements/non_signed_in_links'
 	end
  end
+ def nav_contact_requests_partial_path
+  # if contact requests exist
+  if current_user.pending_received_contact_requests.present? 
+    'layouts/navigation/header/dropdowns/contact_requests/requests' 
+  else # contact requests do not exist 
+    'layouts/navigation/header/dropdowns/contact_requests/no_requests'
+  end
+end
 end
