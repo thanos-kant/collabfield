8c16b3c776c59aa19de01dae2ff8be6c263535df
thanos-kant <thanos.kantalaft@gmail.com>
2019-02-07 18:33:34 +0200

    Render a drop down menu of conversations links

--- app/controllers/private/conversations_controller.rb
+++ app/controllers/private/conversations_controller.rb
@@ -28,6 +28,13 @@ class Private::ConversationsController < ApplicationController
         format.js
       end
     end
+    def open
+      @conversation = Private::Conversation.find(params[:id])
+      add_to_conversations unless already_added?
+      respond_to do |format|
+        format.js { render partial: 'private/conversations/open' }
+      end
+    end
 
     private
 
