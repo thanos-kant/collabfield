9bb410f6b0dd4b1ee48716d8fba20bcf3fa0e247
thanos-kant <thanos.kantalaft@gmail.com>
2019-02-15 03:25:44 +0200

    Add JavaScript to messenger.js to show a different

--- app/assets/javascripts/messenger.js
+++ app/assets/javascripts/messenger.js
@@ -7,4 +7,20 @@ $(document).on('turbolinks:load ajax:complete', function() {
         $('.load-more-messages', this)[0].click();
         $('.conversation .messages-list .loading-more-messages').hide();
     }
+
+
+// if the messenger is opened on a smaller screen device
+// show the messenger's version for mobile devices
+    $(".messenger .col-sm-2 ul").on( "click", "a", function() {
+        var col_2_width = $('.messenger .col-sm-2').css('width');
+        var window_width = '' + $('.messenger').width() + 'px';
+        // check if bootstrap columns are stacked (page is opened on a smaller device)
+        if (col_2_width == window_width) {
+            $('body nav').hide();
+            $('.messenger .col-sm-2').hide();
+            $('.messenger .col-sm-10').show();
+            $('body').css('margin', '0 0 0 0');
+            $('.messenger').css('height', '100vh');
+        }
+    });
 });
