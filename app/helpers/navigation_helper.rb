8fddb8d4e55ca0edf431ec1570aa5392108a22d5
thanos-kant <thanos.kantalaft@gmail.com>
2019-02-15 03:47:39 +0200

    - Create a _contact_requests.html.erb inside the

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -1,4 +1,5 @@
 module NavigationHelper
+  
  def collapsible_links_partial_path
     if user_signed_in?
       'layouts/navigation/collapsible_elements/signed_in_links'
@@ -6,14 +7,7 @@ module NavigationHelper
       'layouts/navigation/collapsible_elements/non_signed_in_links'
 	end
  end
- def nav_contact_requests_partial_path
-  # if contact requests exist
-  if current_user.pending_received_contact_requests.present? 
-    'layouts/navigation/header/dropdowns/contact_requests/requests' 
-  else # contact requests do not exist 
-    'layouts/navigation/header/dropdowns/contact_requests/no_requests'
-  end
-end
+ 
 # render the navigation header's content
 def nav_header_content_partials
   partials = []
@@ -34,5 +28,14 @@ end
     else
       'layouts/navigation/header/dropdowns/conversations/group'
     end  
+  end
+
+  def nav_contact_requests_partial_path
+    # if contact requests exist
+    if current_user.pending_received_contact_requests.present? 
+      'layouts/navigation/header/dropdowns/contact_requests/requests' 
+    else # contact requests do not exist 
+      'layouts/navigation/header/dropdowns/contact_requests/no_requests'
+    end
   end
 end
